92d19d1cd5da9fb1486d53bc97c444714a3e5aa1
Riccardo Fracescato <riccardo.francescato@gmail.com>
2016-12-15 11:32:25 +0100

    added example of multiple reg mod

--- linear_reg.r
+++ linear_reg.r
@@ -27,8 +27,13 @@ cat(paste0(" Regression  SS: ",SSR," Df: ", 1 ," MS: ",MSR, " F0: ",F0,
 		"\n Total  SS: ",SST," Df: ", (length(X)-1)))
 
 
+mydata = data.frame(
+  y = c(2256,2340,2426,2293,2330,2368,2250,2409,2364,2379,2440,2364,2404,2317,2309,2328),
+  x1 = c(80,93,100,82,90,99,81,96,94,93,97,95,100,85,86,87),
+  x2 = c(8,9,10,12,11,8,8,10,12,11,13,11,8,12,9,12)
+)
 # Multiple Linear Regression Example 
-fit <- lm(y ~ x1 + x2 + x3, data=mydata)
+fit <- lm(y ~ x1 + x2, data=mydata)
 summary(fit) # show results
 # Other useful functions 
 coefficients(fit) # model coefficients
